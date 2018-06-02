7844ab221a7cab6884c6ce1bcef46976e9e67c97
myj <xiamu@juxiaoyun.cn>
2018-06-02 20:55:31 +0800

    添加注释 - 事件详情

--- routes/api/eventDetail.js
+++ routes/api/eventDetail.js
@@ -11,10 +11,13 @@ module.exports = function(req, res, next) {
     per_page: parseInt(_query.per_page),
     current_page: parseInt(_query.page)
   }
+  // 查询事件详情总页数
   mysql.select([], `SELECT COUNT(*) total FROM measuring_point WHERE event_id = ${_query.id}`).then(result => {
     pagination.total = result[0].total
+    // 查询事件信息
     mysql.select([], `SELECT * FROM measuring_event WHERE id = ${_query.id}`).then(eventData => {
       data.event = eventData
+      // 查询事件详情分页
       mysql.select([], `SELECT * FROM measuring_point WHERE event_id = ${_query.id} ORDER BY id ASC LIMIT ${_query.per_page}`).then(item => {
         data.detail = item
         res.send({data, pagination})
