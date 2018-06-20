6ed8d83d28d72c9b245c084cb2ae73e6672cd4cf
myj <xiamu@juxiaoyun.cn>
2018-06-20 19:49:31 +0800

    查询优化

--- routes/api/measureEvent.js
+++ routes/api/measureEvent.js
@@ -8,9 +8,36 @@ module.exports = function(req, res, next) {
     per_page: parseInt(_query.per_page),
     current_page: parseInt(_query.page)
   }
-  mysql.select([], `SELECT COUNT(*) total FROM measuring_event`).then(result => {
+  let searchTime
+  let searchWord
+
+  // 时间查询
+  (_query.start_time || _query.end_time)
+    ? searchTime = `(update_time BETWEEN '${_query.start_time}' AND '${_query.end_time}' OR measure_time BETWEEN '${_query.start_time}' AND '${_query.end_time}')`
+    : searchTime = ''
+  // 关键字查询
+  _query.search
+    ? searchWord = `(site = '${_query.search}' OR instrument = '${_query.search}' OR operator = '${_query.search}')`
+    : searchWord = ''
+
+  // 查询搜索总条数
+  mysql.select([], `
+    SELECT COUNT(*) total FROM measuring_event
+    ${(searchTime ? 'WHERE ' : '') + searchTime}
+    ${(searchWord ? searchTime ? 'AND ' : 'WHERE ' : '') + searchWord}
+  `).then(result => {
     pagination.total = result[0].total
-    mysql.select([], `SELECT * FROM measuring_event WHERE id >= (SELECT id FROM measuring_event LIMIT ${_query.per_page * (_query.page -1)}, 1) ORDER BY id ASC LIMIT ${_query.per_page}`).then(item => {
+    // 查询搜索分页
+    mysql.select([], `
+      SELECT * FROM measuring_event
+      WHERE id >= (
+        SELECT id FROM measuring_event LIMIT ${_query.per_page * (_query.page -1)}, 1
+      )
+      ${(searchTime ? 'AND' : '') + searchTime}
+      ${(searchWord ? 'AND' : '') + searchWord}
+      ORDER BY id ASC
+      LIMIT ${_query.per_page}
+    `).then(item => {
       data = item
       res.send({data, pagination})
     })
